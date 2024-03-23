4dcda739afab9abc5e8a14e540fde11b8e47de33
StealthM0de <jedongfallorina@gmail.com>
2024-03-23 23:33:23 +0800

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-// prettier-ignore
-import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, persistStore } from 'redux-persist';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistStore,
+} from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
